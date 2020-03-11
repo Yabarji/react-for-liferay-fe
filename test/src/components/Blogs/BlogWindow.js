d08d2c4346899b8335177f0314c0d4eaee3967e6
Setup <setup@Setups-MacBook-Pro.local>
2020-03-11 11:25:47 +0900

    RFE-01 | docs : react js in home directory!

---
+++ test/src/components/Blogs/BlogWindow.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class BlogWindow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    
+    }
+  }
+
+  render() {
+    return (
+      <div className="blogContainer">
+       <div>Hello!</div>
+      </div>
+    )
+  }
+}
+
